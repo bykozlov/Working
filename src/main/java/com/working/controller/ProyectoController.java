08434cffc577f48947045e3f5d6c495d500fce91
bykozlov <saavedraaaron89@gmail.com>
2023-10-28 12:33:28 -0500

    aa

--- src/main/java/com/working/controller/ProyectoController.java
+++ src/main/java/com/working/controller/ProyectoController.java
@@ -80,16 +80,30 @@ public class ProyectoController {
 		return "registro";
 	}
 
-	@GetMapping("/index/login")
+	@GetMapping("/login")
 	public String cargarPaginaLogin() {
 		return "login";
 	}
+
+	/*@PostMapping("/login")
+	public String login(@RequestParam("txtCorreoIn") String correo_usu, @RequestParam("txtPasswordIn") String contra_usu,
+			Model model) {
+		Registro usuario = repoRegistro.findByCorreo_usu(correo_usu);
+
+		if (usuario != null && usuario.getContra_usu().equals(contra_usu)) {
+			return "redirect:/dashboard";
+		} else {
+			model.addAttribute("error", "Credenciales inválidas. Inténtalo de nuevo.");
+			return "login";
+		}
+	}*/
 
 	@GetMapping("/CarreraTec")
 	public String redireccionarACarreraTec() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "CarreraTec";
 	}
+
 	@GetMapping("/CarreraUni")
 	public String redireccionarACarreraUni() {
 		// Realiza cualquier lógica adicional aquí si es necesario
@@ -101,90 +115,106 @@ public class ProyectoController {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasTecnicas/carrtc1";
 	}
+
 	@GetMapping("/Carrtc2")
 	public String carretec2() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasTecnicas/carrtc2";
 	}
+
 	@GetMapping("/Carrtc3")
 	public String carretec3() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasTecnicas/carrtc3";
 	}
+
 	@GetMapping("/Carrtc4")
 	public String carretec4() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasTecnicas/carrtc4";
 	}
+
 	@GetMapping("/Carrtc5")
 	public String carretec5() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasTecnicas/carrtc5";
 	}
+
 	@GetMapping("/Carrtc6")
 	public String carretec6() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasTecnicas/carrtc6";
 	}
+
 	@GetMapping("/Carrtc7")
 	public String carretec7() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasTecnicas/carrtc7";
 	}
+
 	@GetMapping("/Carrtc8")
 	public String carretec8() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasTecnicas/carrtc8";
 	}
+
 	@GetMapping("/Carrtc9")
 	public String carretec9() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasTecnicas/carrtc9";
 	}
+
 	@GetMapping("/CarrUn1")
 	public String carreuni1() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasUniversitarias/carreUni1";
 	}
+
 	@GetMapping("/CarrUn2")
 	public String carreuni2() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasUniversitarias/carreUni2";
 	}
+
 	@GetMapping("/CarrUn3")
 	public String carreuni3() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasUniversitarias/carreUni3";
 	}
+
 	@GetMapping("/CarrUn4")
 	public String carreuni4() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasUniversitarias/carreUni4";
 	}
+
 	@GetMapping("/CarrUn5")
 	public String carreuni5() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasUniversitarias/carreUni5";
 	}
+
 	@GetMapping("/CarrUn6")
 	public String carreuni6() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasUniversitarias/carreUni6";
 	}
+
 	@GetMapping("/CarrUn7")
 	public String carreuni7() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasUniversitarias/carreUni7";
 	}
+
 	@GetMapping("/CarrUn8")
 	public String carreuni8() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasUniversitarias/carreUni8";
 	}
+
 	@GetMapping("/CarrUn9")
 	public String carreuni9() {
 		// Realiza cualquier lógica adicional aquí si es necesario
 		return "/webSite/carrerasUniversitarias/carreUni9";
 	}
 }
-
