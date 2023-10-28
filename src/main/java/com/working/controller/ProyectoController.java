b4db69766b6a03cbee995a565849176969af98c2
andresmoreno0709 <morenomonjajesusandres@gmail.com>
2023-10-28 14:14:52 -0500

    login

--- src/main/java/com/working/controller/ProyectoController.java
+++ src/main/java/com/working/controller/ProyectoController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.working.model.Registro;
 import com.working.model.Tipo;
+import com.working.model.Usuarios;
 import com.working.repository.IRegistroRepository;
 import com.working.repository.ITipoRepository;
+import com.working.repository.IUsuarioRepository;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -24,6 +26,11 @@ public class ProyectoController {
 
 	@Autowired
 	ITipoRepository repoTipo;
+	
+	@Autowired
+	private IUsuarioRepository repoUsu;
+	
+
 
 	// controlador para cargar la pagina
 	@GetMapping("/index")
@@ -78,14 +85,36 @@ public class ProyectoController {
 
 		return "registro";
 	}
-
+/*
 	@GetMapping("/index/login")
 	public String cargarPaginaLogin() {
 		return "login";
 	}
-	 
-	/*@GetMapping("/login")
-    public String cargarPaginaIndex() {
-        return "login";
-    }*/
+	*/
+	 //login
+	@GetMapping("/login")
+	public String cargarPaginaLogin(Model model) {
+	    // Aquí, si lo deseas, puedes agregar el modelo necesario para cargar la página de inicio de sesión.
+	    model.addAttribute("usuario", new Usuarios());
+	    return "login";
+	}
+
+	@GetMapping("/logout")
+	public String cargarPaginaLogout(Model model) {
+	    model.addAttribute("usuario", new Usuarios());
+	    return "login";
+	}
+
+	@PostMapping("/login")
+	public String ingresar(Model model, @ModelAttribute Usuarios usuario) {
+	    Usuarios u = repoUsu.findByCorreoAndContraseña(usuario.getCorreo(), usuario.getContraseña());
+	    if (u != null) {
+	        return "registro";
+	    } else {
+	        model.addAttribute("mensaje", "contraseña o correo incorrecto");
+	        model.addAttribute("clase", "alert alert-danger");
+	        return "redirect:bienvenida";
+	    }
+	}
+
 }
--- src/main/java/com/working/model/Usuarios.java
+++ src/main/java/com/working/model/Usuarios.java
@@ -1,13 +1,12 @@
 package com.working.model;
 
-
-
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Entity
@@ -17,11 +16,11 @@ public class Usuarios {
 
 	@Id
 	private int cod_usuario;
-
-	private String contraseña;
-	
+	 @NotEmpty
 	private String correo;
-	
+	@NotEmpty
+	private String contraseña;
+	@NotNull
 	private int id_tipo;
 	
 
